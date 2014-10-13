7e5568bc5cdc723f237c117c28f9e9c68bd78e0d
Tim Smith <tim@cozy.co>
2014-10-13 13:31:06 -0700

    Switch from Rakefile's to reagan_test.yml file defining tests
    Rake doesn't return 1 when tests fail

--- Gemfile
+++ Gemfile
@@ -3,7 +3,4 @@ source 'https://rubygems.org'
 gem 'octokit', '~> 3.0'
 gem 'chef', '~> 11.0'
 gem 'ridley', '~> 4.0'
-
-group :test do
-  gem 'rake', '~> 10.0'
-end
+gem 'rake', '~> 10.0'
--- lib/test_reagan.rb
+++ lib/test_reagan.rb
@@ -16,23 +16,33 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-# tests cookbooks using the reagan-test rake tasks
+# tests cookbooks using tests defined in reagan_test.yml files
 class TestReagan < Reagan
   def initialize(cookbook)
     @cookbook = cookbook
   end
 
-  # performs knife cookbook test
-  # returns  true if  cookbook passed or false if it failed
+  # returns  true if tests defined in reagan_test.yml passed/don't exist or false if it failed
   def test
-    puts 'Running reagan_test Rake task:'
-    # run the rake task if it exists, otherwise skip
-    if File.exist?(File.join(@@config['jenkins']['workspace_dir'], 'cookbooks', @cookbook, 'Rakefile'))
-      result = system "rake #{File.join(@@config['jenkins']['workspace_dir'], 'cookbooks', @cookbook, 'Rakefile')} > /dev/null 2>&1"
-      puts result ? '    PASS: reagan_test Rake task was successful' : '    FAIL: reagan_test Rake task was NOT successful'
-      result
+    puts 'Running reagan_test.yml defined tests:'
+    # check to see if a reagan_test.yml file exists
+    if File.exist?(File.join(@@config['jenkins']['workspace_dir'], 'cookbooks', @cookbook, 'reagan_test.yml'))
+
+      # load the reagan config file
+      reagan_def = YAML.load_file(File.join(@@config['jenkins']['workspace_dir'], 'cookbooks', @cookbook, 'reagan_test.yml'))
+
+      # change into the cookbook dir so rake tests run locally
+      Dir.chdir(File.join(@@config['jenkins']['workspace_dir'], 'cookbooks', @cookbook))
+
+      pass = true
+      reagan_def['tests'].each do |test|
+        result = system test
+        pass = false if result == false
+      end
+      puts pass ? '    PASS: reagan_test.yml test was successful' : '    FAIL: reagan_test.yml test was NOT successful'
+      pass
     else
-      puts '    SKIP: No Rakefile found in the cookbook path. Skipping test'
+      puts '    SKIP: No reagan_test.yml file found in the cookbook path. Skipping test'
     end
     true
   end
